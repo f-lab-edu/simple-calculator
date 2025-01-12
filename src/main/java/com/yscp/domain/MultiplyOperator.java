120c411f7190f5e8953abc390253ba94dab91a22
yushin <kkad45@naver.com>
2025-01-12 23:48:41 +0900

    feature/1 곱하기 기능을 구현할 수 있다.

---
+++ src/main/java/com/yscp/domain/MultiplyOperator.java
@@ -0,0 +1,12 @@
+package com.yscp.domain;
+
+public class MultiplyOperator implements Operator {
+    @Override
+    public Integer operate(Integer number1, Integer number2) {
+        if (number1 == null || number2 == null) {
+            throw new IllegalArgumentException("Null numbers not allowed");
+        }
+
+        return number1 * number2;
+    }
+}
---
+++ src/test/java/com/yscp/domain/MultiplyOperatorTest.java
@@ -0,0 +1,46 @@
+package com.yscp.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("MultiplyOperator")
+class MultiplyOperatorTest {
+
+    private Operator operator;
+
+    @BeforeEach
+    void setUp() {
+        operator = new MultiplyOperator();
+    }
+
+    @ParameterizedTest
+    @DisplayName("두 숫자를 곱할 수 있다.")
+    @CsvSource(value = {"4,-4,-16", "2,2,4", "-4,-4,16"})
+    void itReturnsMultiplyNumber(Integer number1, Integer number2, Integer result) {
+        Assertions.assertThat(operator.operate(number1, number2)).isEqualTo(result);
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("nullParameters")
+    @DisplayName("숫자 중 null 값이 들어올 경우 예외를 던진다.")
+    void itThrowsNullPointerExceptionWhenNumberIsNull(Integer number1, Integer number2) {
+        Assertions.assertThatThrownBy(() -> operator.operate(number1, number2));
+    }
+
+    public static Stream<Arguments> nullParameters() {
+        return Stream.of(Arguments.of(null, 1),
+                Arguments.of(1, null),
+                Arguments.of(null, null));
+    }
+}
